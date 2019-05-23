eb814243909c5bb6e3ba10147a401306716bf561
JayLim <ddx180874@gmail.com>
2019-05-23 12:22:52 +0400

    in progress: delete issues & projects

--- src/main/resources/static/js/deleteRequestSender.js
+++ src/main/resources/static/js/deleteRequestSender.js
@@ -11,3 +11,18 @@ function deleteRequest(id, entity) {
         document.getElementById("row-" + id).innerHTML = null;
     });
 }
+
+function deleteOperativeDataRequest(id, entity) {
+    $.post({
+        url: '/' + entity + '/delete/' + id,
+        statusCode: {
+            200: function () {
+                console.log("Row # " + id + " has been removed (" + entity + ").");
+            }
+        }
+    }).done(function (data) {
+        document.getElementById("response").innerHTML += data;
+        document.getElementById("row-" + id).innerHTML = null;
+        document.getElementById("info-block" + id).innerHTML = "<div class=\"issue-body-frame no-content\">Запись удалена<p style=\"font-size:12pt;\">Восстановление невозможно.</p></div>";
+    });
+}
